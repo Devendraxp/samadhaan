9ff82277ad42dc2278d66e45327453eb55e08630
Devendra <dnowtech@gmail.com>
2025-11-15 23:44:53 +0530

    fixed cores

--- server/src/app.js
+++ server/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'http://localhost:3001',process.env.FRONTEND_URL],
+    origin: true,
     credentials: true,
   })
 );
